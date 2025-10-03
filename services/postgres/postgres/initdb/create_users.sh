0f17cc157274176412b19c3b99b1c52f6872f197
Ionescu Liviu Cristian <ionescu.liviucristian@yahoo.com>
2025-10-03 14:06:22 +0300

    chore: remove unused code

--- services/postgres/postgres/initdb/create_users.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-set -eu
-
-PGPASSWORD="$(cat /run/secrets/POSTGRES_PASSWORD)"
-export PGPASSWORD
-
-psql -v ON_ERROR_STOP=1 --username "postgres" <<-EOSQL
-  CREATE USER ${POSTGRES_SERVICES_USER} WITH CREATEDB PASSWORD '$(cat /run/secrets/POSTGRES_SERVICES_USER_PASSWORD)';
-EOSQL
