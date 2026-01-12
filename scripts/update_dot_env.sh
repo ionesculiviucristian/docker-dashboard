813d6f4e4fe5957ad479f1e1f3fe619a3e4a6912
Ionescu Liviu Cristian <ionescu.liviucristian@yahoo.com>
2026-01-12 19:07:10 +0200

    fix: keep ollama gpu config when updating

--- scripts/update_dot_env.sh
+++ scripts/update_dot_env.sh
@@ -5,7 +5,19 @@ set -eu
 source "./scripts/helpers.sh"
 
 if [ -f "./.env" ]; then
-    awk -F= '
+    gpu_config=""
+
+    if grep -q "^AI_PROFILE=" "./.env"; then
+        current_ai_profile=$(grep "^AI_PROFILE=" "./.env" | cut -d= -f2-)
+        # shellcheck disable=SC2016
+        if echo "${current_ai_profile}" | grep -q '${OLLAMA_NVIDIAGPU}'; then
+            gpu_config=':${OLLAMA_NVIDIAGPU}'
+        elif echo "${current_ai_profile}" | grep -q '${OLLAMA_AMDGPU}'; then
+            gpu_config=':${OLLAMA_AMDGPU}'
+        fi
+    fi
+
+    awk -v gpu_config="${gpu_config}" -F= '
         NR==FNR {
             if (/^[A-Z_]/ && $2 != "\"\"")
                 e[$1] = substr($0, index($0, "=") + 1)
@@ -13,6 +25,12 @@ if [ -f "./.env" ]; then
         }
         /^[A-Z_][A-Z0-9_]*=""$/ && $1 in e {
             print $1 "=" e[$1]
+            next
+        }
+        /^AI_PROFILE=/ && gpu_config != "" {
+            value = substr($0, index($0, "=") + 1)
+            gsub(/"$/, gpu_config "\"", value)
+            print $1 "=" value
             next
         }
         { print }
